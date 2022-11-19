01e34f92212c731e8744e77fec1b02cffe4eaf4f
Iryna <irinka25041990@gmail.com>
2022-11-19 14:09:03 +0100

    TestEuro:aggiunt metodi:testGetValore,testMinoreDi

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,5 +1,9 @@
 package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,42 +13,42 @@ import code.Business_logic.Euro;
 
 public class TestEuro {
 
-    private Euro euroTest, euroTest1, euroTest2, euroTest3,euroTest4;
-    
+    private Euro euroTest, euroTest1, euroTest2, euroTest3, euroTest4;
+
     @Before
-    public void setUp(){
-        euroTest = new Euro(125,55);
-        euroTest1 = new Euro(129,99);
-        euroTest2 = new Euro(6,20);
-        euroTest3 = new Euro(25,25);
-        euroTest4 = new Euro(77,30);
+    public void setUp() {
+        euroTest = new Euro(125, 55);
+        euroTest1 = new Euro(129, 99);
+        euroTest2 = new Euro(6, 20);
+        euroTest3 = new Euro(25, 25);
+        euroTest4 = new Euro(77, 30);
     }
-    
+
     @After
-    public void clear(){
+    public void clear() {
         euroTest = null;
         assertNull(euroTest);
     }
 
-
     @Test
     public void testGetValore() {
-
+        assertEquals(euroTest.getValore(), 12555, 0.0);
     }
 
     @Test
     public void testMinoreDi() {
-
+        assertTrue(euroTest.minoreDi(euroTest));
+        assertFalse(euroTest1.minoreDi(euroTest));
     }
 
     @Test
     public void testSomma() {
-
+        
     }
 
     @Test
     public void testSottrai() {
-
+        
     }
 
     @Test
